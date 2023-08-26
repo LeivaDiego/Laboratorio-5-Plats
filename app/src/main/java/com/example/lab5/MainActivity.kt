35c64b259952b6d9ede3e002d43261bbfd593edf
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-25 23:12:47 -0600

    v5.0 Main Menu navigation
    First version of the 4 buttons for navigation on the main activity

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.lab5
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab5.ui.theme.Lab5Theme
 
@@ -37,10 +42,31 @@ fun MainMenuPreview() {
 }
 
 @Composable
-fun MainMenu(modifier: Modifier = Modifier) {
-    Text(
-        text = "Bienvenido a la App Interactiva",
-        modifier = modifier
-    )
-}
+fun MainMenu() {
+    val mContext = LocalContext.current
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Button(onClick = { mContext.startActivity(Intent(mContext, CampusInfo::class.java)) })
+        {
+            Text("Ir a Campus")
+        }
+
+        Button(onClick = { mContext.startActivity(Intent(mContext, MyProfile::class.java)) })
+        {
+            Text("Ir a Mi Perfil")
+        }
+
+        Button(onClick = { mContext.startActivity(Intent(mContext, Settings::class.java)) })
+        {
+            Text("Ir a Configuración")
+        }
+
+        Button(onClick = { mContext.startActivity(Intent(mContext, Emergency::class.java)) })
+        {
+            Text("Ir a Emergencias")
+        }
+    }
+}
