7fa117e30d0bffaa49dc6b0b0548c8e21f3a77c2
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-25 22:50:57 -0600

    v3.0 Settigns update
    Settings.kt restored to its lab 4 state

--- app/src/main/java/com/example/lab5/Settings.kt
+++ app/src/main/java/com/example/lab5/Settings.kt
@@ -3,13 +3,30 @@ package com.example.lab5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab5.ui.theme.Lab5Theme
 
 class Settings : ComponentActivity() {
@@ -22,25 +39,169 @@ class Settings : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting3("Android")
+                    SettingsPage()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
+fun SettingsPreview() {
     Lab5Theme {
-        Greeting3("Android")
+        SettingsPage()
+    }
+}
+
+
+@Composable
+fun SettingsPage(){
+    Surface {
+        val prof = painterResource(id = R.drawable.profile)
+        val mail = painterResource(id = R.drawable.email)
+        val notif = painterResource(id = R.drawable.notifications)
+        val help = painterResource(id = R.drawable.help)
+        val about = painterResource(id = R.drawable.info)
+        val priv = painterResource(id = R.drawable.privacy)
+        val back = painterResource(id = R.drawable.back)
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth())
+        {
+            Row(horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth())
+            {
+                Image(painter = back,
+                    contentDescription = "Back Arrow",
+                    modifier = Modifier
+                        .absoluteOffset(0.dp, 16.dp)
+                        .size(40.dp))
+
+                Text(text = "Configuración",
+                    style = TextStyle(color = Color.Black, fontSize = 26.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(20.dp))
+
+                Text(text = "       ")
+            }
+
+            Column(horizontalAlignment = Alignment.Start)
+            {
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Row {
+                    Image(painter = prof,
+                        contentDescription = "profile settings logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Text(text = "Editar Perfil",
+                        style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                        modifier = Modifier.padding(20.dp))
+                }
+
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Row {
+                    Image(painter = mail,
+                        contentDescription = "email settings logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Text(text = "Correos Electrónicos",
+                        style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                        modifier = Modifier.padding(20.dp))
+                }
+
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Row {
+                    Image(painter = notif,
+                        contentDescription = "notifications settings logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Text(text = "Notificaciones",
+                        style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                        modifier = Modifier.padding(20.dp))
+                }
+
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Row {
+                    Image(painter = priv,
+                        contentDescription = "privacy settings logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Text(text = "Privacidad",
+                        style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                        modifier = Modifier.padding(20.dp))
+                }
+
+                Divider(color = Color.LightGray, thickness = 20.dp)
+
+                Row {
+                    Image(painter = help,
+                        contentDescription = "help logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Column {
+                        Text(text = "Ayuda y Comentarios",
+                            style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(top = 10.dp))
+
+                        Text(text = "Solución de problemas y Guías",
+                            style = TextStyle(color = Color.Black, fontSize = 15.sp),
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(bottom = 10.dp))
+                    }
+                }
+
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Row {
+                    Image(painter = about,
+                        contentDescription = "About logo",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .absoluteOffset(0.dp, 15.dp))
+
+                    Column {
+                        Text(text = "Información",
+                            style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(top = 10.dp))
+
+                        Text(text = "Información y Documentación de la App",
+                            style = TextStyle(color = Color.Black, fontSize = 15.sp),
+                            modifier = Modifier
+                                .padding(horizontal = 20.dp)
+                                .padding(bottom = 10.dp))
+                    }
+                }
+
+                Divider(color = Color.LightGray, thickness = 20.dp)
+
+                Text(text = "Cerrar Sesión",
+                    style = TextStyle(color = Color.Red, fontSize = 20.sp, FontWeight.Bold),
+                    modifier = Modifier.padding(20.dp),
+                    textAlign = TextAlign.Center)
+
+                Divider(color = Color.Gray, thickness = 1.dp)
+
+                Divider(color = Color.LightGray, thickness = 500.dp)
+            }
+        }
     }
 }
